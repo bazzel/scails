0ccc6d9ad736ff41ec16df31bb05c06d70d339fa
bazzel <patrick.baselier@gmail.com>
2024-03-24 11:51:34 +0100

    Play correct octave

--- app/javascript/controllers/player_controller.js
+++ app/javascript/controllers/player_controller.js
@@ -5,41 +5,51 @@ export default class extends Controller {
   static values = {
     modeNumber: Number,
   };
+  static targets = ["note"];
 
   initialize() {
     this.isPlaying = false;
   }
 
-  playNote() {
+  playScale() {
     if (!this.isPlaying) {
       const synth = new Tone.Synth({
         onsilence: () => (this.isPlaying = false),
       }).toDestination();
       const now = Tone.now();
+      const allNotes = this.allNotes;
       const notes = this.playableNotes();
       const length = 0.2;
       const subdivision = "8n";
+      const c4 = allNotes.indexOf("C");
+      let octaveNumber;
 
       this.isPlaying = true;
-
+      
       for (const [index, element] of notes.entries()) {
-        const note = `${element}4`;
+        const chromaticNote = allNotes.indexOf(element);
+        octaveNumber = (chromaticNote < c4) ? 3 : 4;
+        
+        const note = `${element}${octaveNumber}`;
         const time = now + index * length;
+
         synth.triggerAttackRelease(note, subdivision, time);
       }
 
-      const note = `${notes[0]}5`;
+      if (c4 == 0) octaveNumber = 5;
+      const note = `${notes[0]}${octaveNumber}`;
       const time = now + notes.length * length;
       synth.triggerAttackRelease(note, subdivision, time);
     }
   }
 
   playableNotes() {
+    const allNotes = this.allNotes;
     let arr = [];
 
     for (const [index, element] of this.binaryScale.entries()) {
       if (element == 1) {
-        arr.push(this.allNotes[index]);
+        arr.push(allNotes[index]);
       }
     }
 
@@ -51,6 +61,6 @@ export default class extends Controller {
   }
 
   get allNotes() {
-    return ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"];
+    return this.noteTargets.map((element) => element.textContent);
   }
 }
