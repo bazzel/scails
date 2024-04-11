b2a7481efca93cb3c6eaa18b4d9287b0574cba89
bazzel <patrick.baselier@gmail.com>
2024-04-11 15:32:35 +0200

    Show balloon

--- app/javascript/controllers/slider_controller.js
+++ app/javascript/controllers/slider_controller.js
@@ -1,16 +1,21 @@
 import { Controller } from "@hotwired/stimulus";
 
-const lightTextColor = "text-primary-100";
-const darkTextColor = "text-primary-500";
-
 export default class extends Controller {
-  static targets = ["input", "stopIndicator"];
+  static targets = ["input", "stopIndicator", "balloon"];
 
   connect() {
+    this.updateValues();
+  }
+
+  updateValues() {
     this.updateProgressIndicator();
+    this.updateBalloon();
   }
 
   updateProgressIndicator() {
+    const lightTextColor = "text-primary-100";
+    const darkTextColor = "text-primary-500";
+
     const ratio = this.ratio;
     const stopIndicatorTargets = this.stopIndicatorTargets;
 
@@ -26,6 +31,12 @@ export default class extends Controller {
     });
   }
 
+  updateBalloon() {
+    const offset = `calc(${this.ratioAsPercentage})`;
+    this.balloonTarget.textContent = this.inputTarget.value;
+    this.balloonTarget.style.setProperty("--offset", offset);
+  }
+
   get ratio() {
     const range = this.inputTarget.max - this.inputTarget.min;
     const ratio = (this.inputTarget.value - this.inputTarget.min) / range;
@@ -35,5 +46,11 @@ export default class extends Controller {
 
   get ratioAsPercentage() {
     return `${this.ratio * 100}%`;
+  }
+
+  toggleBalloon() {
+    this.balloonTarget.classList.toggle("opacity-0");
+    this.balloonTarget.classList.toggle("scale-50");
+    this.balloonTarget.classList.toggle("translate-y-3");
   }
 }
