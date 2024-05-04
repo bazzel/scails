f310cd109cb5844505c1206bb0d12c29e4d46c6b
bazzel <patrick.baselier@gmail.com>
2024-05-04 14:33:36 +0200

    Show on hover

--- app/javascript/controllers/pin_controller.js
+++ app/javascript/controllers/pin_controller.js
@@ -1,16 +1,20 @@
 import { Controller } from "@hotwired/stimulus";
 
 const pinnedKey = "pinned";
+const visibilityClass = "!visible";
 
 export default class extends Controller {
-  static targets = ["checkbox"];
+  static targets = ["checkbox", "pinContainer"];
   static values = {
     modeNumber: Number,
   };
   static classes = ["shown", "hidden"];
 
   connect() {
-    this.checkboxTarget.checked = this.#isPinned();
+    if (this.#isPinned()) {
+      this.checkboxTarget.checked = true;
+      this.#makeVisible();
+    }
   }
 
   addOrRemovePin() {
@@ -19,8 +23,10 @@ export default class extends Controller {
 
     if (this.#isPinned()) {
       pinned.splice(index, 1);
+      this.#makeInvisible();
     } else {
       pinned.push(this.modeNumberValue);
+      this.#makeVisible();
     }
 
     localStorage.setItem(pinnedKey, JSON.stringify(pinned));
@@ -30,11 +36,19 @@ export default class extends Controller {
     return this.#pinIndex() > -1;
   }
 
-  #getPinned() {
-    return JSON.parse(localStorage.getItem(pinnedKey)) || [];
-  }
-
   #pinIndex() {
     return this.#getPinned().indexOf(this.modeNumberValue);
+  }
+
+  #makeVisible() {
+    this.pinContainerTarget.classList.add(visibilityClass);
+  }
+
+  #makeInvisible() {
+    this.pinContainerTarget.classList.remove(visibilityClass);
+  }
+
+  #getPinned() {
+    return JSON.parse(localStorage.getItem(pinnedKey)) || [];
   }
 }
