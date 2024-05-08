6101f6b83970bc35a11290b0c6126b5dedb41d94
bazzel <patrick.baselier@gmail.com>
2024-05-08 17:07:35 +0200

    Reset form when not saving

--- app/javascript/controllers/modal_component_controller.js
+++ app/javascript/controllers/modal_component_controller.js
@@ -4,31 +4,29 @@ export default class extends Controller {
   static targets = ["dialog", "backdrop", "form"];
   static classes = ["visible", "hidden"];
 
-  toggle(event, skipCheck = false) {
-    if (
-      skipCheck ||
-      event.key === "Escape" ||
-      event.target === event.currentTarget
-    ) {
-      this.dialogTarget.classList.toggle(this.hiddenClass);
-      this.backdropTarget.classList.toggle(this.hiddenClass);
-      this.dialogTarget.classList.toggle(this.visibleClass);
-      this.backdropTarget.classList.toggle(this.visibleClass);
-    }
+  toggle() {
+    this.dialogTarget.classList.toggle(this.hiddenClass);
+    this.backdropTarget.classList.toggle(this.hiddenClass);
+    this.dialogTarget.classList.toggle(this.visibleClass);
+    this.backdropTarget.classList.toggle(this.visibleClass);
   }
 
   open(event) {
     if (this.dialogTarget.classList.contains(this.hiddenClass)) {
-      this.toggle(event, true);
+      this.toggle();
     }
   }
 
   close(event) {
-    event.preventDefault();
+    if (event.key === "Escape" || event.target === event.currentTarget) {
+      event.preventDefault();
 
-    if (this.dialogTarget.classList.contains(this.visibleClass)) {
-      this.toggle(event, true);
-      location.reload();
+      if (this.dialogTarget.classList.contains(this.visibleClass)) {
+        this.toggle();
+
+        this.formTarget.reset();
+        // location.reload();
+      }
     }
   }
 
